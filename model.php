06a38c65323aec9102d209a2daad0561e717a555
Abdelilah0107 <abdelilah.elkorchi1999@gmail.com>
2021-05-03 12:26:57 +0000

    Database Connection

--- model.php
+++ model.php
@@ -1,8 +1,23 @@
  <?php
+ /* Database Connection*/
 
+ class Model{
+     private $servername = 'localhost';
+     private $username = 'root';
+     private $password = '';
+     private $dbname = 'blog';
+     private $conn;
 
+    public function __construct(){
+         $this->conn = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
+         if($this->conn->connect_error){
+             echo 'Connection Failed';
+         }else{
+             echo 'Connected';
+         }
+     }
+}
 
-
-
+new Model();
 
 ?>
