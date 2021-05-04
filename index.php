faeee1f1f0af863d47baf5aba4142f46a01376a3
Abdelilah0107 <abdelilah.elkorchi1999@gmail.com>
2021-05-04 15:02:29 +0000

    Update table

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 include 'model.php';
 $obj = new Model();
+// insert table
 if(isset($_POST['submit'])){
      $obj->insertTable($_POST);
 }
+// update table
+if(isset($_POST['update'])){
+    $obj->updateTable($_POST);
+}
+if(isset($_GET['deleteid'])){
+    $delid = $_GET['deleteid'];
+    $obj->deleteTable($delid);
+}
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -19,6 +31,44 @@ if(isset($_POST['submit'])){
 <body>
     <h2 class="text-center text-info">Blog Add Person</h2>
     <div class="container">
+    <?php
+
+    if(isset($_GET['msg']) AND $_GET['msg']=='ins'){
+        echo '<div class="alert alert-primary" role="alert">
+             Table Inserted Successfully..!
+             </div>';
+    }
+
+    if(isset($_GET['msg']) AND $_GET['msg']=='ups'){
+        echo '<div class="alert alert-primary" role="alert">
+             Table Updated Successfully..!
+             </div>';
+    }
+    ?>
+    <?php
+          // Update
+    if(isset($_GET['editid'])){
+           $editid = $_GET['editid'];
+           $mytable = $obj->showTableById($editid);
+           ?>     
+    <!-- Update -->
+    <form action="index.php" method="post">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" value="<?php echo $mytable['name'];?>" placeholder="Enter Your Name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" value="<?php echo $mytable['email'];?>" placeholder="Enter Your Email" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="hidden" name="hid" value="<?php echo $mytable['id'];?>">
+                <input type="submit" name="update" value="Update" class="btn btn-info">
+            </div>
+    </form>
+    <?php
+        }else{
+    ?>
         <form action="index.php" method="post">
             <div class="form-group">
                 <label>Name</label>
@@ -32,6 +82,7 @@ if(isset($_POST['submit'])){
                 <input type="submit" name="submit" value="Submit" class="btn btn-info">
             </div>
         </form>
+        <?php  }    ?>
         <h2 class="text-center text-danger">Create table</h2>
         <table class="table table-bordered">
              <tr class="bg-primary text-center">
@@ -40,7 +91,25 @@ if(isset($_POST['submit'])){
                 <th>Email</th>
                 <th>Action</th>
              </tr>  
-     </div>
+             <?php 
+            //  show table
+            $data = $obj->showTable();
+            $sno=1;
+            foreach($data as $value){
+                ?>
+            <tr class="text-center">
+               <td><?php echo $sno++; ?></td>
+               <td><?php echo $value['name']; ?></td>
+               <td><?php echo $value['email']; ?></td>
+               <td>
+                  <a href="index.php?editid=<?php echo $value['id'];?>" class="btn btn-info">Edit</a>
+                  <a href="index.php?deleteid=<?php echo $value['id'];?>" class="btn btn-danger">Delete</a>
+               </td>
+            </tr>
+            <?php
+            }
+
+             ?>
         </table>
     </div>
 </body>
--- model.php
+++ model.php
@@ -17,7 +17,7 @@
          }
      }
 
-     /*function defie for insert table */
+     /*function define for insert table */
      public function insertTable($post){
          $name = $post['name'];
          $email = $post['email'];
@@ -27,6 +27,46 @@
              header('location:index.php?msg=ins');
          }else{
              echo "Error".$sql."<br>".$this->conn->error;
+         }
+     }
+// function define for update table
+     public function updateTable($post){
+        $name = $post['name'];
+        $email = $post['email'];
+        $editid =$post['hid'];
+        $sql = "UPDATE users SET name='$name' , email='$email' WHERE id='$editid'";
+        $result = $this->conn->query($sql);
+        if($result){
+            header('location:index.php?msg=ups');
+        }else{
+            echo "Error".$sql."<br>".$this->conn->error;
+        }
+    }
+// function define for delete
+
+    public function deleteTable($delid){
+        $sql = "DELET FROM users WHERE id='$delid'"
+        $result = 
+    }
+
+
+
+     public function showTable(){
+         $sql = "SELECT * FROM users";
+         $result = $this->conn->query($sql);
+         if($result->num_rows>0){
+             while($row = $result->fetch_assoc()){
+                 $data[] = $row;
+             }
+             return $data;
+         }
+     }
+     public function showTableById($editid){
+         $sql = "SELECT * FROM users WHERE id =$editid";
+         $result = $this->conn->query($sql);
+         if($result->num_rows==1){
+             $row = $result->fetch_assoc();
+             return $row;
          }
      }
 }
